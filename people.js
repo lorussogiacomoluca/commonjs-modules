54b346841f3dd37febab13f0dcb21a7bd0de2586
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-04-14 15:36:25 +0200

    [UPD] createPerson function

--- people.js
+++ people.js
@@ -1,3 +1,12 @@
 const fullNameFunction = require('./names')
 const hobbiesFunction = require('./hobbies')
 
+function createPerson(){
+    const fullName = fullNameFunction('Mario','Rossi')
+    const hobbies = hobbiesFunction('Swim', 'Run', 'Gym')
+    return{
+        fullName,
+        hobbies
+    }
+}
+
