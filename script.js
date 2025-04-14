31c79a6501f7764433acd54253113e0baa9c52bf
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-04-14 14:59:28 +0200

    [UPD]  Testing hobbies function

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ const hobbies = require('./hobbies')
 //Names function
 const person1 = names('Mario', 'Rossi')
 console.log(person1)
+
+//Hobbies function
+const hobbie1 = hobbies('Swim', 'Run', 'Gym')
+console.log(hobbie1)
